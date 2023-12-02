cc23a556466f65417613a9b7b4452d7a5d516e92
mahme001 <minhaz.ahmed@student.hs-rm.de>
2023-12-02 12:31:53 +0100

    initial commit

--- day01/day01.ts
+++ day01/day01.ts
@@ -1,36 +1,81 @@
 import fs from "fs";
 
-function readFile(): string {
-  return fs.readFileSync("./day01/part01.txt", "utf-8").toString();
+function readFile(path: string): string {
+  return fs.readFileSync(path, "utf-8").toString();
   // return fs.readFileSync("./day01/part02.txt", "utf-8").toString();
 }
 
+const words = [
+  ["zero","zero0zero"],
+  ["one","one1one"],
+  ["two","two2two"],
+  ["three","three3three"],
+  ["four","four4four"],
+  ["five","five5five"],
+  ["six","six6six"],
+  ["seven","seven7seven"],
+  ["eight","eight8eight"],
+  ["nine","nine9nine"]
+];
+
 function solution() {
-  const lines = readFile().split("\n");
+  const linesPart1 = readFile("./day01/part02.txt").split("\n");
+  const linesPart2 = readFile("./day01/part02.txt").split("\n");
+
+  let part1: number =  0;
+  let part2: number = 0;
 
   let strNumber: string = "";
-  let result: number = 0;
-  for (const line of lines) {
+  
+  for (const line of linesPart1) {
     for (const char of line) {
       if (!Number.isNaN(parseInt(char))) {
-        console.log(char);
+        // console.log(char);
         strNumber += char;
       }
     }
 
-    console.log("strNumber", strNumber);
     if (strNumber.length === 0) {
       // do nothing
     } else if (strNumber.length === 1) {
       strNumber += strNumber;
-      result += parseInt(strNumber);
+      part1 += parseInt(strNumber);
     } else {
-      result += parseInt(strNumber[0] + strNumber[strNumber.length - 1]);
+      part1 += parseInt(strNumber[0] + strNumber[strNumber.length - 1]);
     }
     strNumber = "";
   }
 
-  console.log(result);
+
+  strNumber = "";
+
+  for (let line of linesPart2) {
+    strNumber = "";
+    
+    for(const [word, replaceWord] of words) {
+      line = line.replaceAll(word, replaceWord);
+    }
+
+    for (const char of line) {
+      if (!Number.isNaN(parseInt(char))) {
+        strNumber += char;
+      }
+    }
+
+    if (strNumber.length === 0) {
+      // do nothing
+    } else if (strNumber.length === 1) {
+      strNumber += strNumber;
+      part2 += parseInt(strNumber);
+    } else {
+      part2 += parseInt(strNumber[0] + strNumber[strNumber.length - 1]);
+    }
+
+    strNumber = "";
+  }
+
+  console.log(part1);
+  console.log(part2);
 }
 
 solution();
